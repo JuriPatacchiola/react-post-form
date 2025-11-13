d44fc3b4f0899ee5e10e573079a9ff88f6664a61
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-13 16:18:03 +0100

    add the use ìstate for to take informaction whit the link

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 
 
 function App() {
+  const API_URL = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts';
 
+  const app = () => {
+    const [postData, setPostData] = useState({
+      author: '',
+      title: '',
+      body: '',
+      public: false,
+    })
+  }
   return (
     <>
+
+
 
     </>
   )
